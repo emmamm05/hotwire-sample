b76062a34c80a63b05d3ba07474d43f242a7bdc4
Emmanuel Mora <emmanuelmora05@gmail.com>
2022-09-30 20:50:35 -0600

    Add simple Turbo Frame & Turbo Streams examples

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,17 @@
 
 Rails.application.routes.draw do
   resources :posts
+  get "home/index"
+  get "other/index"
+
+  get "/home/turbo_frame_form" => "home#turbo_frame_form", as: "turbo_frame_form"
+  post "/home/turbo_frame_submit" => "home#turbo_frame_submit", as: "turbo_frame_submit"
+
+  post "/other/post_something" => "other#post_something", as: "post_something"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "home#index"
 end
