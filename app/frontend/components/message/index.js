ef1fdf094c8a5737de6aa3bbcd0a98ba7d601c1a
Emmanuel Mora <emmanuelmora05@gmail.com>
2022-10-02 16:59:34 -0600

    Remove stimulus request

--- app/frontend/components/message/index.js
+++ app/frontend/components/message/index.js
@@ -6,18 +6,4 @@ export default class Controller extends BaseController {
   connect() {
     console.log("Loaded Controller")
   }
-
-  destroy(event) {
-    const http = new XMLHttpRequest();
-    const href = event.target.getAttribute("data-href");
-    const csrf_token = event.target.getAttribute("data-csrf-token");
-    http.open('DELETE', href);
-    http.setRequestHeader('X-CSRF-Token', csrf_token);
-    http.setRequestHeader('Content-Type', 'application/json');
-    http.setRequestHeader('Accept', 'application/json')
-    http.onreadystatechange = (e) => {
-      console.log(http.responseText)
-    }
-    http.send();
-  }
 }
