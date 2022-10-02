ac02bd257cd359d2e1d1fee9704675f55f518e55
Emmanuel Mora <emmanuelmora05@gmail.com>
2022-10-02 16:58:20 -0600

    Remove comments

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,43 +3,36 @@ class MessagesController < ApplicationController
 
   before_action :set_message, only: %i[destroy]
 
-  # GET /messages or /messages.json
   def index
     @messages = Message.all.order(created_at: :desc)
   end
 
-  # POST /messages or /messages.json
   def create
     @message = Message.create!(body: params[:body])
 
     Broadcast::Message.new(@message).prepend
 
     respond_to do |format|
-      format.html { redirect_to messages_path }
       format.turbo_stream
     end
   end
 
-  # DELETE /messages/1 or /messages/1.json
   def destroy
     @message.destroy!
 
     Broadcast::Message.new(@message).remove
 
     respond_to do |format|
-      format.html { redirect_to messages_url, notice: I18n.t("messages.destroy.success") }
       format.turbo_stream
     end
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_message
     @message = Message.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def message_params
     params.fetch(:message, {})
   end
