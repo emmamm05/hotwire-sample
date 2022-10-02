bbc72f95114cf5c2acb20643a9ca2a94da256320
Emmanuel Mora <emmanuelmora05@gmail.com>
2022-10-02 02:42:13 -0600

    Add eager and lazy load options

--- app/frontend/components/message/index.js
+++ app/frontend/components/message/index.js
@@ -2,11 +2,8 @@
 // We reserve Controller for the export name
 import { Controller as BaseController } from "@hotwired/stimulus";
 
-export class Controller extends BaseController {
+export default class Controller extends BaseController {
   connect() {
     console.log("Loaded Controller")
   }
 }
-
-Stimulus.register("message", Controller)
-
