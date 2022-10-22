6af7a2a6ab56e43b5ea3f15db6bc448ac9550fd0
Emmanuel Mora <emmanuelmora05@gmail.com>
2022-10-22 17:25:11 -0600

    misc

--- app/frontend/components/nav/index.js
+++ app/frontend/components/nav/index.js
@@ -2,8 +2,18 @@
 // We reserve Controller for the export name
 import { Controller as BaseController } from "@hotwired/stimulus";
 
+const IdSelectorsByPath = {
+  "/messages": "nav-item-messages",
+  "/queries": "nav-item-queries",
+  "/": "nav-item-messages",
+};
+
 export default class Controller extends BaseController {
   connect() {
-    console.log(window.location.pathname);
+    this.selectNavItem(IdSelectorsByPath[window.location.pathname]);
+  }
+
+  selectNavItem(id) {
+    console.log(id);
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   resources :messages, only: [:create, :index, :destroy]
+  get :queries, to: "messages#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
